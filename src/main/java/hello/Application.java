505b5013f13825665dd1abb62bde811a5b60d43f
张延强 <zhangyanqiang@zhangyanqiangdeMacBook-Pro.local>
2017-01-06 22:45:41 +0800

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * Created by zhangyanqiang on 2017/1/6.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static  void main(String args[]){
+
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,29 @@
+package hello;
+
+/**
+ * Created by zhangyanqiang on 2017/1/6.
+ */
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static  final String template = "Hello,%s";
+
+    private static AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name",defaultValue = "World") String name){
+
+        return  new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
+
+
+
+
+}
